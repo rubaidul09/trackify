01550817e155d2a7521ae450df50987d65c18aa5
rubaidul09 <rubaidulmuaz09@gmail.com>
2026-09-09 01:25:43 +0600

    Update home_screen.dart
     update home screen UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+const HomeScreen({super.key});
+
+@override
+Widget build(BuildContext context) {
+return Scaffold(
+
+// Green App Bar
+appBar: AppBar(
+backgroundColor: Colors.green,
+foregroundColor: Colors.white,
+title: const Text(
+'Home',
+style: TextStyle(
+fontWeight: FontWeight.bold,
+),
+),
+centerTitle: true,
+
+leading: IconButton(
+icon: const Icon(Icons.menu),
+onPressed: () {},
+),
+
+actions: [
+IconButton(
+icon: const Icon(Icons.notifications),
+onPressed: () {},
+),
+],
+),
